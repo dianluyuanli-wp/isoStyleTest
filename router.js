27e43cc6654438df71f237c02f8e56f21bc81969
xiaohuli <waterdoor@foxmail.com>
2020-03-31 10:23:10 +0800

    fix: 完善描述

--- router.js
+++ router.js
@@ -5,7 +5,11 @@ const ReactSSR = require('react-dom/server');
 const fs = require('fs');
 const path = require('path');
 
+//  后端渲染的模板文件
 const template = fs.readFileSync(path.join(__dirname,'./view/server.html'),'utf8');
+//  后端渲染需要插入的css内容
+const cssContent = fs.readFileSync(path.join(__dirname,'./dist/serverEntry.css'));
+//  后端渲染打包好的react组件代码
 const ServerEntry = require('./dist/serverEntry.bundle.js');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
@@ -13,13 +17,13 @@ router.get('/', async function(req, res, next) {
     //  后端渲染
     const instance = React.createElement(ServerEntry.default);
     const appString = ReactSSR.renderToString(instance);
-    res.send(template.replace('<app></app>',appString));
+    res.send(template.replace('<app></app>',appString).replace('<style></style>', `<style>${cssContent}</style>`));
+  } else {
+    //前端渲染
+    //  入口文件位置 ./entry/index.js 
+    //  html文件 ./view/index.html
+    res.render('index');
   }
-
-  //前端渲染
-  //  入口文件位置 ./entry/index.js 
-  //  html文件 ./view/index.html
-   res.render('index');
 });
 
 module.exports = router;
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -10,6 +10,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        //  server端跑的必须是commonjs
         libraryTarget: 'commonjs2',
         publicPath: path.resolve(__dirname, 'dist')
     },
